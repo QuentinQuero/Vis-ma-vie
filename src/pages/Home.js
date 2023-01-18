7cb5af2093cbba24dc64d86c5dee223e67c764be
Quentin <quentin2.quero@epitech.eu>
2023-01-18 16:46:34 +0100

    rm component / add comments and help

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+export function Home (){
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+//todo transformer cette page pour en faire une liste de pokemon cliquable avec redirection sur la nouvelle page '/pokemon/:name'
--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -2,3 +2,5 @@
     const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0';
     return fetch(apiUrl)
 }
+
+//todo fonction qui renvoie seulement un pokemon avec son nom en paramètre // voir la doc de l'api
