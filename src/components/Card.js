7c3172684e75d9385d54bb4acc074fff9addb4d9
Quentin <quentin2.quero@epitech.eu>
2023-01-18 16:54:52 +0100

    add types

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 
 export function Card (props){
+    const types = props?.pokemon.types.map(type => <li key={"type"+type.slot}>{type.type.name}</li>)
+
     return props ? (
         <div className="card">
             <img src={props.pokemon.sprites.front_default} alt={props.pokemon?.name}></img>
             <div className="card-body">
                 <h5 className="card-title">{props.pokemon.name}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of
-                    the card's content.</p>
+                <ul>{types}</ul>
             </div>
         </div>
     ) : (<div>Loading</div>)
